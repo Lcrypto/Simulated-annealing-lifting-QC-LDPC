e21df5969ccd4ffb5ee0cd8832919cf10c73b708
USA_VS <L@lcrypto.com>
2019-09-22 01:01:39 +0300

    Update run.bat

--- simulatedAnnealingEMD/x64/Release/run.bat
+++ simulatedAnnealingEMD/x64/Release/run.bat
@@ -1,1 +1,1 @@
-simulatedAnnealingEMD.exe -file proto.txt -circulant 500 -upGirth 8 -emd 20 -seed 123 -numberOfMatrices 1 -girth 8
+simulatedAnnealingEMD.exe -file proto.txt -circulant 500 -upGirth 8 -emd 20 -seed 123 -numberOfMatrices 1 -girth 8 
